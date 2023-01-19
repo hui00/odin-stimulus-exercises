8374e9a4380690f574631fd8a70a100a508843e2
hui00 <kevinbrinkschulte@hotmail.de>
2023-01-19 02:02:48 +0100

    Clicking an element will hide the clicked element and show another

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,20 +1,28 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+
   static classes = ["hidden", "change"];
-  static targets = ["text"];
-
+  static targets = ["text", "dropdown", "hiddenelement"];
   connect() {}
-
   toggle() {
     // only works with one class
     this.textTarget.classList.toggle(this.hiddenClass);
   }
-
   add() {
     this.textTarget.classList.add(...this.changeClasses);
   }
   remove() {
     this.textTarget.classList.remove(...this.changeClasses);
+  }
+
+  dropdowntoggle() {
+    this.dropdownTarget.classList.toggle(this.hiddenClass);
+  }
+
+  hideandshow(event) {
+    // hide option 1 and show option 4
+    event.target.classList.add(this.hiddenClass);
+    this.hiddenelementTarget.classList.remove(this.hiddenClass);  
   }
 }
